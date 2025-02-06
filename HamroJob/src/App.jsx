51e2893014be0fcbe561484bf088c30a59b7d568
unknown <np03cs4a220138@heraldcollege.edu.np>
2025-02-06 14:52:43 +0545

    🎨 style(Homepage): improve UI  and make it responsive as well

--- HamroJob/src/App.jsx
+++ HamroJob/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./component/Signup";
 import Login from "./component/Login";
+import Homepage from"./component/Homepage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/" element={<Login />} />
+        
       </Routes>
     </Router>
   );
--- HamroJob/src/component/Homepage.jsx
+++ HamroJob/src/component/Homepage.jsx
@@ -1,20 +1,60 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Signup from "./component/Signup";
-import Login from "./component/Login";
+import "./Homepage.css";
+
+
 
 function Homepage() {
   return (
    <>
-   <div className="container">
-<div className="homepage"></div>
+ <div className="Homepage_container">
+      <div className="homepage">
+         
+        <div className="nav">
+          <div className="logo">
+            <img className="Logo" src="/src/assets/Logo.png" alt="Job Portal Logo" />
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#">Jobs</a></li> 
+              <li><a href="#">About Us</a></li>
+            </ul>
+          </nav>
+
+          <div className="nav-controls">
+            <div className="search-icon">
+            <ion-icon name="search-outline" className="search-outline"></ion-icon>
+            </div>
+            <button className="signin-button">SignIn</button>
+            <button className="login-button">LogIn</button>
+            </div>
+           </div>
+
+           <section className="hero">  
+          <div className="hero-content">
+            <h1>Get ready with Hamro Job</h1>
+            <p>Find your future job, internship, or training</p>
+            <div className="search-bar">
+              <input type="text" placeholder="Profession, company, training..." />
+              <button>Search</button> 
+            </div>
+          </div>
+          <div className="hero-image"><img src="/src/assets/HeroImage.jpeg" alt="" /></div>
+        </section>
+     
+
+      </div>
+    </div>
+
 <div className="jobsection"></div>
 <div className="About_us_section"></div>
 <div className="skillcontent_section"></div>
 
-   </div>
+
    </>
+
   );
 }
+
 
 export default Homepage;
